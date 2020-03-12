b004d245942c4e50592459a1e9b63cb865105caf
CedThen <frawg.lehgs@gmail.com>
2020-03-12 14:12:39 -0400

    test commit

--- src/Components/Madlibs/MadlibsMain.js
+++ src/Components/Madlibs/MadlibsMain.js
@@ -9,6 +9,7 @@ import ThreeJsBg from "../ThreeJSBackground.js";
 
 // const socketURL = "ws://localhost:3030";
 const socketURL = "ws://ec2-3-14-133-230.us-east-2.compute.amazonaws.com:3030";
+const h = "";
 
 class MadlibsHome extends React.Component {
   constructor() {
